ed1b532cf4f1622575530f44ed58f4bd4ba22683
Gioant <66393141+Gioant@users.noreply.github.com>
2021-01-02 00:57:22 -0500

    Update AdditionFormula.java

--- part01-Part01_19.AdditionFormula/src/main/java/AdditionFormula.java
+++ part01-Part01_19.AdditionFormula/src/main/java/AdditionFormula.java
@@ -14,6 +14,8 @@ public class AdditionFormula {
         int second = Integer.valueOf(scanner.nextLine());
         
         System.out.println(first + " + " + second + " = " + (first + second));
-        
+        // output values in variables called first & second
+        // the + and = without quotation marks just appends next to the variables.
+        // finally, in the parenthesis output the total from the variables first + second.
     }
 }
