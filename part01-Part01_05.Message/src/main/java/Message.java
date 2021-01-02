5a0c6da1c3e26ede6a551080537bb166d33f71b4
Gioant <66393141+Gioant@users.noreply.github.com>
2021-01-01 23:13:41 -0500

    Update Message.java

--- part01-Part01_05.Message/src/main/java/Message.java
+++ part01-Part01_05.Message/src/main/java/Message.java
@@ -6,12 +6,19 @@ public class Message {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
+        //write program here
+        
         System.out.println("Write a message:");
+        // output following text written inside the parentheses
         
         String words = scanner.nextLine();
+        /* create a data type for text called String (in java language). 
+           store it inside a variable called words.
+           scan for user input and go to next line.
+        */
         
         System.out.println(words);
-       
+       // Output the stored value in words.
 
     }
 }
