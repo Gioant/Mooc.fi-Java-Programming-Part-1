d5d7641546bd926da8c9f8628d08441ad3357d3c
Gioant <66393141+Gioant@users.noreply.github.com>
2021-01-01 23:17:08 -0500

    Update Greeting.java

--- part01-Part01_08.Greeting/src/main/java/Greeting.java
+++ part01-Part01_08.Greeting/src/main/java/Greeting.java
@@ -6,11 +6,15 @@ public class Greeting {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Write your program below
+        
         System.out.println("What's your name?");
+        // Ask user for their name
         
         String name = scanner.nextLine();
+        // Scan & store their answer in a variable called name.
         
-        System.out.println("Hi " + name + "!");
-
+        System.out.println("Hi " + name);
+        // output a static "Hi" and the answer they gave before.
     }
 }
